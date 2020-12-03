ae4d64f5c4c5d03e9d189fc60fa454d4d6bd45ad
Dmitry Nezvinskyi <Nezvinskyi@users.noreply.github.com>
2020-12-03 15:48:34 +0100

    Create tasks-7-kyu.js

---
+++ js/tasks-7-kyu.js
@@ -0,0 +1,13 @@
+
+// ----> Given an integral number, determine
+// if it's a square number
+
+// var isSquare = function (n) {
+//   if (Number.isInteger(Math.sqrt(n))) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log('isSquare :>> ', isSquare(25));
