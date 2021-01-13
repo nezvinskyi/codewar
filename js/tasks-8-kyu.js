0f9d54d34bafd5646de0574cb6f11636958a4c77
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-01-13 16:59:26 +0100

    Update tasks-8-kyu.js

--- js/tasks-8-kyu.js
+++ js/tasks-8-kyu.js
@@ -228,3 +228,17 @@
 // console.log('makeNegative(12) :>> ', makeNegative(12));
 // console.log('makeNegative(12) :>> ', makeNegative(0));
 // console.log('makeNegative(12) :>> ', makeNegative(-12));
+
+
+// --------> N-th Power
+// You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+// function nthPower(arr, n) {
+// 	if (n > arr.length-1) {
+// 		return -1
+// 	} else {
+// 		return Math.pow(arr[n], n)
+// 	}
+// }
+
+// console.log(nthPower([1, 2, 3, 4, 5], 2)); //result is 3^2 == 9;
+// console.log(nthPower([1, 2, 3], 3)); // N is outside of the array, so the result is -1.
