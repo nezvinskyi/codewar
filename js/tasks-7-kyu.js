100fc3f5eb1f80beec5ed730617107fc9507a49b
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-01-17 15:23:38 +0100

    Update tasks-7-kyu.js

--- js/tasks-7-kyu.js
+++ js/tasks-7-kyu.js
@@ -84,3 +84,97 @@
 // console.log(getCount('o a kak ushakov lil vo kashu kakao'));
 // console.log(getCount('my pyx'));
 
+// ------> Mumbling
+// function accum(s) {
+// 	// let toArray = (s.split(''));
+// 	// let newArray = [];
+// 	// for (let i = 0; i < toArray.length; i++) {
+// 	// 	let newWord = [s[i].toUpperCase()];
+// 	// 	for (let j = 1; j <= i; j++) {
+// 	// 		newWord.push(s[i].toLowerCase())
+// 	// 	}
+// 	// 	newArray.push(newWord.join(''));
+// 	// }
+// 	// return newArray.join('-')
+
+// 	/*
+// 	*  shorter version
+// 	*/
+// 	return s.split('').map((value, index) => (value.toUpperCase() + value.toLowerCase().repeat(index))).join('-');
+// }
+
+// console.log(accum("abcd")); //-> "A-Bb-Ccc-Dddd"
+// console.log(accum("RqaEzty")); //-> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// console.log(accum("cwAt")); //-> "C-Ww-Aaa-Tttt"
+
+// ------> Count the Digit
+// function nbDig(n, d) {
+// 	let squaredDigits = [];
+// 	let counter = 0;
+// 	for (let i = 0; i <= n; i++){
+// 		squaredDigits.push(i*i)
+// 	}
+// 	let newStr = squaredDigits.join('').split('');
+// 	newStr.reduce((acc, value) => {
+// 		if (value == d) {
+// 			counter += 1;
+// 			return acc;
+// 		}
+// 	}, 0)
+// 	return counter
+// }
+// console.log(nbDig(5750, 0)); // 4700
+
+// ------> Ce*s*r*d Strings
+// function uncensor(infected, discovered) {
+// 	let infectedArray = [...infected];
+// 	let discoveredArray = [...discovered];
+// 	let counter = 0;
+// 	for (let i = 0; i < infectedArray.length; i++){
+// 		if (infectedArray[i] === '*') {
+// 			infectedArray[i] = discoveredArray[counter];
+// 			counter += 1;
+// 		};
+// 	}
+//   return infectedArray.join('');
+// }
+// console.log(uncensor("*h*s *s v*ry *tr*ng*", "Tiiesae")); // ➜ "This is very strange"
+// console.log(uncensor("A**Z*N*", "MAIG")); // ➜ "AMAZING"
+// console.log(uncensor("xyz", "")); // ➜ "xyz"
+
+// ------> Even numbers in an array
+
+// function evenNumbers(array, number) {
+// 	// let newArray = [];
+// 	// for (let i = array.length; i >= 0; i--){
+// 	// 	if (array[i] % 2 === 0 && newArray.length <= (number-1)) {
+// 	// 		newArray.unshift(array[i]);
+// 	// 	} 
+// 	// }
+// 	// return newArray;
+
+// 	/*
+// 	* shorter version 
+// 	*/	
+// 	return array.filter(item=>item%2===0).slice(-number)
+// }
+
+// console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)); // => [4, 6, 8]
+// console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2)); // => [-8, 26]
+// console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1)); // => [6]
+
+// ------> 
+
+function apparently(string) {
+	let arr = string.split(' ')
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i + 1] !== 'apparently' && arr[i] === 'but' || arr[i + 1] !== 'apparently' && arr[i] === 'and' ) {
+			arr.splice(i + 1, 0, 'apparently');
+		}
+	}
+	return arr.join(' ')
+}
+
+console.log(apparently("It was great and I've never been on live television before but sometimes I don't watch this."));
+
+console.log(apparently("but apparently"));
